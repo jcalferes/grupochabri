2d851a0792a1be57eacd2c8b490391ce7ed10e24
Angel Solis <Angel Solis@AngelSolis-PC>
2014-01-12 17:16:59 -0600

    modalProducto, falta guardar

--- administracion/mostrarListaPrecios.php
+++ administracion/mostrarListaPrecios.php
@@ -1,12 +1,10 @@
 <?php
-
-echo"algo";
 include './administracion.dao/dao.php';
 $dao = new dao();
-$datos = $dao->consultarMarcas();
+$datos = $dao->consultarListaPrecios();
 echo '<select>';
 while ($rs = mysql_fetch_array($datos)) {
-    echo'<option>' . $rs[1] . '</option>';
+    echo'<option>' . $rs["1"] . '</option>';
 }
 echo '</select>';
 
