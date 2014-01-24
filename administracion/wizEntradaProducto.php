b61b82c3f1cfa6f29684911bd6e3bf4f7b71bf87
pablo <pablo@pablo-Satellite-Pro-L300>
2014-01-24 12:48:12 -0600

    Existencias

--- administracion/wizEntradaProducto.php
+++ administracion/wizEntradaProducto.php
@@ -3,7 +3,7 @@
 <html lang="es">
     <body>
         <div id="wizard">
-            <h2><span class="glyphicon glyphicon-th-list"/>&numsp;Tabla de Entradas</h2>
+            <h2><span class="glyphicon glyphicon-th-list"/>&numsp;Tabla de Inventarios</h2>
             <section>
                 <div class="col-lg-3" style="margin-left: 3%">
                     <label>Codigo del Producto</label>
@@ -11,6 +11,13 @@
                            type="text" 
                            placeholder="Codigo del producto..."
                            id="codigoProducto"/>
+                    <br>
+                </div> 
+                <br>
+                <div class="col-lg-12" style="margin-left: 3%">
+                    <div id="detalle" class="well well-lg">
+                        Información del producto
+                    </div>
                 </div>
                 <br>
                 <br>
@@ -20,6 +27,14 @@
 
                     </table>
                 </div>
+            </section>
+            <h2><span class="glyphicon glyphicon-th-list"/>&numsp;Entradas Proveedores</h2>
+            <section>
+                
+            </section>
+            <h2><span class="glyphicon glyphicon-th-list"/>&numsp;Transferencias</h2>
+            <section>
+                
             </section>
         </div>
         <script src="../administracion/administracion.js/controlWizard.js"></script>
