6e6a025b41d472d43e6aec8a2498f26a25816b46
Usuario <Usuario@Usuario-PC>
2014-02-14 12:54:35 -0600

    Modulo de Agentes Finalizado

---
+++ dameFila.php
@@ -0,0 +1,20 @@
+<?php
+
+echo ' <input type="button" id="agregar" value="hoolaa" />';
+$interfaz = '<input type="button" id="agregar" value="hoolaa" />';
+return $interfaz;
+
+//
+//echo'<tr id="filaBase" class="fila-base">
+//    <td><input type="text" class="nombre" value="hola como estas "/></td>
+//    <td><input type="text" class="apellidos" /></td>
+//        <td>
+//          <select class="sexo">
+//            <option value="0">- Sexo -</option>
+//            <option value="M">Masculino</option>
+//            <option value="F">Femenino</option>
+//          </select>
+//       </td>
+//        <td class="eliminar">Eliminar</td>
+//    </tr>';
+?>
