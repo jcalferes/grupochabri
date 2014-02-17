c9fd7f606077f8854661772f76817d36b07bde68
Angel Solis <Angel Solis@AngelSolis-PC>
2014-02-17 10:06:03 -0600

    nada

--- administracion/consultarProducto.php
+++ administracion/consultarProducto.php
@@ -2,19 +2,19 @@
 
 include './administracion.dao/dao.php';
 $dao = new dao();
-$datos = $dao->consultaProducto($cn);
+$datos = $dao->consultaProducto();
 $datos2 = $dao->consultaTableConsulta();
-
+$comprobar = "";
 echo"<div class='table-responsive'><table class='table table-hover'><thead><th>Editar</th><th>Producto</th><th>Proveedor</th><th>Marca</th><th>Costo</th><th>En existencia</th><th>Lista de precios</th></thead><tbody>";
 while ($rs = mysql_fetch_array($datos)) {
     echo"<tr ><td><input type='checkbox' id='eliminar' onclick='eliminar()'><input  type='button' id ='detalleTarifa' class='btn btn-primary' data-dismiss='modal' data-toggle='modal' data-target='#mdlDetalleTarifa' value='+' style='display: none;'></td> ";
     echo"<td >$rs[producto]</td>";
-    $comprobar = $rs[codigoProducto];
+    $comprobar = $rs['codigoProducto'];
     echo"<td>$rs[codigoProducto]</td>";
     echo"<td id='$rs[marca]' >$rs[marca] </td>";
     echo"<td id='$rs[costo]' >$rs[costo] </td>";
     while ($rs2 = mysql_fetch_array($datos2)) {
-        if ($comprobar == $rs2[idProducto]) {
+        if ($comprobar === $rs2["idProducto"]) {
             if ($rs2[cantidad] !== "") {
                 echo"<td>$rs2[cantidad]</td>";
             }
