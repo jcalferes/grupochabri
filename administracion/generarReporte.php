4b3b7b312da944f58b68db2fe050f1ef84f9656c
Solis <Solis@HOME.lan>
2014-06-03 11:29:24 -0500

    Merge origin/master
    Conflicts:
    	administracion/generarReporte.php
    	administracion/reportes/probando.pdf
    	dompdf/lib/fonts/log.htm

--- administracion/generarReporte.php
+++ administracion/generarReporte.php
@@ -1,10 +1,11 @@
 <?php
 
+session_start();
 include_once '../dompdf/dompdf_config.inc.php';
 include_once './administracion.dao/dao.php';
 include_once '../utilerias/Utilerias.php';
 # Instanciamos un objeto de la clase DOMPDF. 
-session_start();
+
 $idsucursal = $_SESSION["sucursalSesion"];
 $idsucursal = $_POST["sucursal"];
 $mipdf = new DOMPDF();
@@ -149,7 +150,7 @@ if ($validar > 0) {
 	</STYLE>
     </head>
     <body LANG="es-MX" DIR="LTR">
-<!--        <IMG SRC="administracion.imgs/encabezados_'.$idsucursal.'.png" NAME="Imagen 1" ALIGN="center" HSPACE=12 WIDTH=650 HEIGHT=182 BORDER=0> -->
+<!--        <IMG SRC="administracion.imgs/encabezados_' . $idsucursal . '.png" NAME="Imagen 1" ALIGN="center" HSPACE=12 WIDTH=650 HEIGHT=182 BORDER=0> -->
 <!--        <h4>FOLIO NO.-' . $folio . ' </h4> -->';
     if ($comprobante == "PEDIDO CLIENTE") {
         $valor .= '      <table class="CSSTableGenerator">
@@ -157,9 +158,8 @@ if ($validar > 0) {
         <tr><td>Direccion:<br> "Poner aqui la direccion del cliente"</td><td>Colonia:<br> "Poner aqui la colonia del cliente"</td><td></td></tr>
         <tr style="background-color: white"><td>Localidad/Municipio:<br> "Poner la localidad del cliente"</td><td>Estado:<br> "Poner aqui el estado del cliente"</td><td>CP:<br>"El numero postal del cliente"</td></tr>
         </table>';
-    }
-    else{
-         $valor .= '      <table class="CSSTableGenerator">
+    } else {
+        $valor .= '      <table class="CSSTableGenerator">
         <tr><td>Nombre:<br> "Poner aqui el nombre del cliente"</td><td>RFC:<br> "Poner aqui el RGF del cliente"</td><td>Factura:<br><label style="color: red; font-size: larger">"00000"</label><br>Fecha de emision:<br>"La decha de emision de la factura"</td></tr>
         </table>';
     }
@@ -183,7 +183,7 @@ if ($validar > 0) {
         $sda = $datosOrden["sdaComprobante"];
         $iva = $datosOrden["ivaComprobante"];
         $total = $datosOrden["totalComprobante"];
-        $valor .= '<tr><td style="text-align: right">' . $datosOrden["cantidadConcepto"] . '</td><td style="text-align: right">' . number_format($datosOrden["codigoConcepto"], 2) . '</td><td >' . $datosOrden["descripcionConcepto"] . '</td><td>'. $datosOrden["metrosCubicos"] .'</td><!--<td style="text-align: right">$' . number_format($datosOrden["precioUnitarioConcepto"], 2) . '</td>--><td style="text-align: right">$' . number_format($datosOrden["costoCotizacion"], 2) . '</td><!--<td style="text-align: right">$' . number_format($datosOrden["desctUnoConcepto"], 2) . '</td><td style="text-align: right">$' . number_format($datosOrden["desctDosConcepto"], 2) . '</td><td style="text-align: right">$' . $datosOrden["totalComprobante"] . '</td>--><td style="text-align: right">$' . number_format($datosOrden["cdaConcepto"], 2) . '</td><td style="text-align: right">$' . number_format($datosOrden["importeConcepto"], 2) . '</td></tr>';
+        $valor .= '<tr><td style="text-align: right">' . $datosOrden["cantidadConcepto"] . '</td><td style="text-align: right">' . number_format($datosOrden["codigoConcepto"], 2) . '</td><td >' . $datosOrden["descripcionConcepto"] . '</td><td>' . $datosOrden["metrosCubicos"] . '</td><!--<td style="text-align: right">$' . number_format($datosOrden["precioUnitarioConcepto"], 2) . '</td>--><td style="text-align: right">$' . number_format($datosOrden["costoCotizacion"], 2) . '</td><!--<td style="text-align: right">$' . number_format($datosOrden["desctUnoConcepto"], 2) . '</td><td style="text-align: right">$' . number_format($datosOrden["desctDosConcepto"], 2) . '</td><td style="text-align: right">$' . $datosOrden["totalComprobante"] . '</td>--><td style="text-align: right">$' . number_format($datosOrden["cdaConcepto"], 2) . '</td><td style="text-align: right">$' . number_format($datosOrden["importeConcepto"], 2) . '</td></tr>';
     }
     $valor .= '</table>';
     $valor .= '<div style="position:relative"><br><table class="CSSTableGenerator" style="position:absolute; left:490px; width:30%; "><tr><td>Subtotal:</td><td style="text-align: right">$' . number_format($subtotal, 2) . '</td></tr><tr><td>  Desc. General :</td><td style="text-align: right"> $' . number_format($descGral, 2) . '</td></tr><tr><td> Desc. Productos: </td><td style="text-align: right">$' . number_format($descProd, 2) . '</td></tr><tr><td>  Desc. Total : </td><td style="text-align: right">$' . number_format($descTotal, 2) . '</td></tr><tr><td> SDA :</td><td style="text-align: right">$' . number_format($sda, 2) . '</td></tr><tr><td>  Iva 16% :</td><td style="text-align: right"> $' . number_format($iva, 2) . '</td></tr><tr><td>  Total :</td><td style="text-align: right"> $' . number_format($total, 2) . '</td></tr> </table>';
