7b0df492d7e536245ecc6c82eeda7dbb6e12954e
Solis <Solis@HOME>
2014-06-12 11:36:51 -0500

    reparando descuentos orden compra, probando transferencias

--- administracion/mostrarDetallesRequisicion.php
+++ administracion/mostrarDetallesRequisicion.php
@@ -11,7 +11,7 @@ $sucu = $_GET["sucu"];
 if($aceptacion == "5"){
     
     $datos = $dao->mostrarDetallesTransferencias($sucursal, $transferencia);
-echo"<div class='table-responsive'><table id='tdProducto'  class='table table-hover'><thead><th>codigoProducto</th><th>detalles</th><th>Cantidad a dar</th><th>Cantidad pedida</th><th>Costo</th><th>cantidad en Inventario</th><th>Costo Total</th></thead><tbody>";
+echo"<div class='table-responsive'><table id='tdProducto'  class='table table-hover'><thead><th>codigoProducto</th><th>detalles</th><th>Cantidad aceptada</th><th>Cantidad pedida</th><th>Costo</th><th>cantidad en Inventario</th><th>Costo Total</th></thead><tbody>";
 while ($rs = \mysql_fetch_array($datos, MYSQL_ASSOC)) {
     $cantidad = $rs["cantidad"];
     $costo = $rs["costo"];
@@ -32,7 +32,7 @@ echo "<div class='form-control'>Total:  <input type='text' id='costoTotal2' clas
 }else{
     
      $datos = $dao->mostrarDetallesTransferenciasAceptadas($sucursal, $transferencia);
-echo"<div class='table-responsive'><table id='tdProducto'  class='table table-hover'><thead><th>codigoProducto</th><th>detalles</th><th>Cantidad a dar</th><th>Cantidad pedida</th><th>Costo</th><th>cantidad en Inventario</th><th>Costo Total</th></thead><tbody>";
+echo"<div class='table-responsive'><table id='tdProducto'  class='table table-hover'><thead><th>codigoProducto</th><th>detalles</th><th>Cantidad aceptada</th><th>Costo</th><th>cantidad en Inventario</th><th>Costo Total</th></thead><tbody>";
 while ($rs = \mysql_fetch_array($datos, MYSQL_ASSOC)) {
     $cantidad = $rs["cantidad"];
     $costo = $rs["costo"];
@@ -41,7 +41,7 @@ while ($rs = \mysql_fetch_array($datos, MYSQL_ASSOC)) {
     echo"<tr><td><input type='text' class='myCodigo2 form-control' id='codigo2$rs[codigoProducto]' value='$rs[codigoProducto]' disabled/></td>";
     echo"<td ><input type='text' id='producto$rs[codigoProducto]' class='form-control' value='$rs[producto]' disabled/></td>";
     echo"<td ><div id='div2$rs[codigoProducto]'  class='form-group'><input type='text' id=txtCantidad2$rs[codigoProducto] class='form-control' value='$rs[cantidad]' onblur='sacarTotal2(\"$rs[codigoProducto]\")' disabled></div></td>";
-    echo"<td ><input type='text' id=txtCantidad3$rs[codigoProducto] value='$rs[cantidad]' class='form-control' disabled/></td>";
+//    echo"<td ><input type='text' id=txtCantidad3$rs[codigoProducto] value='$rs[cantidad]' class='form-control' disabled/></td>";
     echo"<td ><input type='text' id='costoUnitario2$rs[codigoProducto]' value='$rs[costo]' class='form-control' disabled/></td>";
     echo"<td><input type='text' id='txtMaxCantidad2$rs[codigoProducto]' value='$rs[cantidadTotal]' class='form-control' disabled/></td>";
     echo"<td ><input type='text' class='requisicion form-control' id='txtTotal2$rs[codigoProducto]' value='$total'  disabled/></td>";
