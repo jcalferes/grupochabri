1190b5b602491c01f8b3867480d4dab4139f8004
Solis <Solis@HOME>
2014-06-12 12:11:04 -0500

    probando transferencias

--- administracion/mostrarDetallesTransferencia.php
+++ administracion/mostrarDetallesTransferencia.php
@@ -26,7 +26,7 @@ if ($aceptacion == '5') {
         
     }
 } else {
-    $datos = $dao->mostrarDetallesRequisicion($sucursal, $transferencia, $sucu);
+    $datos = $dao->mostrarDetallesTransferenciasAceptadas($sucursal, $transferencia);
     echo"<center><input  type='button' id ='detalleTransferencia' class='btn btn-primary' data-dismiss='modal' data-toggle='modal' data-target='#mdlDetalleTransferencia' value='+' style='display: none;'/><button type='button' class='btn btn-xs btn-default' id='btnver' onclick='eliminarProductos()'><span class='glyphicon glyphicon-trash'></span></button></cente><div class='table-responsive'><table id='tdProducto'  class='table table-hover'><thead><th>codigoProducto</th><th>detalles</th><th>Cantidad</th><th>CantidadTotal</th><th>Costo</th></thead><tbody>";
     while ($rs = \mysql_fetch_array($datos)) {
         echo"<tr><td >$rs[codigoProducto]</td>";
