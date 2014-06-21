1d7272813ef3e0d39ebeff5b7d3a2f991e5268e6
Pablo <comodoro>
2014-06-21 13:03:56 -0500

    Buscador actualizado

--- administracion/dameFolioPedidos.php
+++ administracion/dameFolioPedidos.php
@@ -4,6 +4,11 @@ $idSucursal = $_SESSION["sucursalSesion"];
 include './administracion.dao/dao.php';
 $dao = new dao();
 $rs = $dao->dameFolioOrdenCompra($idSucursal);
+if($rs == true){
 while ($datos = mysql_fetch_array($rs)) {
     echo '<label style = "color: red; font-size: 15px">' . $datos[0] . '</label>';
+}
+}
+else{
+    echo mysql_error();
 }
